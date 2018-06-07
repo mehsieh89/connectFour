992da6e6f0e9f8e74de9dc7831599f55f780c438
Mel hsieh <mehsieh89@gmail.com>
2018-06-07 16:12:31 -0700

    added diagonal left and right logic

--- src/logic.js
+++ src/logic.js
@@ -6,7 +6,7 @@ let board = [
   [0, 1, 1, 1, 0, 1, 0],
   [0, 0, 1, 0, 0, 1, 0]
 ];
-
+//checks a row
 function rowCheck(row) {
   let tracker = {
     red: 0,
@@ -28,7 +28,7 @@ function rowCheck(row) {
     }
   }
 }
-
+//checks all rows
 function rowCheckAll(board) {
   for (let i = 0; i < board.length; i++) {
     let check = rowCheck(board[i]);
@@ -37,7 +37,7 @@ function rowCheckAll(board) {
     }
   }
 }
-
+//checks a column
 function colCheck(board) {
   let columnIndex = 0;
   for (let k = 0; k < board[0].length; k++) {
@@ -62,4 +62,96 @@ function colCheck(board) {
     }
     columnIndex++;
   }
+}
+//checks diagonals from Left, using array conversion then row check;
+function rowCheck(row) {
+  let tracker = {
+    red: 0,
+    black: 0
+  };
+  for (let k = 0; k < row.length; k++) {
+    if (row[k] === 0) {
+      tracker.red++;
+      tracker.black = 0;
+      if (tracker.red === 4) {
+        return "red is the winner";
+      }
+    } else if (row[k] === 1){
+      tracker.black++;
+      tracker.red = 0;
+      if (tracker.black === 4) {
+        return "black is the winner";
+      }
+    }
+  }
+}
+
+function diagLeftCheck(board) {
+  let columnIndex = 0;
+  let rowIndex = 2;
+  let output;
+
+  for (let i = 0; i < 6; i++) {
+    function diagonalConversion(rowPos, colPos) {
+      let arrayToCheck = [];
+      while(board[rowPos] !== undefined) {
+        if (board[rowPos][colPos] === undefined) {
+          rowPos++;
+          colPos++;
+          continue;
+        } else {
+          arrayToCheck.push(board[rowPos][colPos]);
+          rowPos++;
+          colPos++;
+        }
+      }
+      if (output === undefined) {
+        output = rowCheck(arrayToCheck);
+      }
+      console.log(arrayToCheck);
+    }
+    diagonalConversion(rowIndex, columnIndex);
+    if (rowIndex !== 0) {
+      rowIndex--;
+    }
+     if (rowIndex === 0) {
+      columnIndex++;
+    }
+  }
+  return output;
+}
+//checks diagonals from Right, using array conversion then row check;
+function diagRightCheck(board) {
+  let columnIndex = 6;
+  let rowIndex = 2;
+  let output;
+  
+  for (let i = 0; i < 6; i++) {
+    function diagonalConversion(rowPos, colPos) {
+      let arrayToCheck = [];
+      while(board[rowPos] !== undefined) {
+        if (board[rowPos][colPos] === undefined) {
+          rowPos++;
+          colPos--;
+          continue;
+        } else {
+          arrayToCheck.push(board[rowPos][colPos]);
+          rowPos++;
+          colPos--;
+        }
+      }
+      if (output === undefined) {
+        output = rowCheck(arrayToCheck);
+      }
+      console.log(arrayToCheck);
+    }
+    diagonalConversion(rowIndex, columnIndex);
+    if (rowIndex !== 0) {
+      rowIndex--;
+    }
+    if (rowIndex === 0) {
+      columnIndex--;
+    }
+  }
+  return output;
 }
